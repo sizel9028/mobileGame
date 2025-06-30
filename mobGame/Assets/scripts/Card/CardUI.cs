07c855e439c8de7f174fb4619121aa13166a3f15
Koo seungwoon <pinkfish7913@gmail.com>
2025-06-30 17:23:55 +0900

    card 바꿈

--- mobGame/Assets/scripts/Card/CardUI.cs
+++ mobGame/Assets/scripts/Card/CardUI.cs
@@ -10,7 +10,7 @@ public class CardUI : MonoBehaviour
     public LocalizedText descriptionText;
     public Button cardButton;
 
-    private CardData data;
+    //private CardData data; 카드 데이터는 나중에 manager가 관리하자
 
     public void SetCard(CardData newData)
     {
